40adf63cb402a1280aba966294d9a069ee35b8c6
Eduardo Costa <eduardocastro.jj@gmail.com>
2015-02-17 11:11:15 -0200

    updated git ignore file and added 'MinutoSeguros.BlogFeed.Log' project.

---
+++ MinutoSeguros.BlogFeed/MinutoSeguros.BlogFeed.Log/ILogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MinutoSeguros.BlogFeed.Log
+{
+    public interface ILogger
+    {
+        void Info(string message);
+        void Error(string message);
+        void Error(string message, Exception exception);
+    }
+}
---
+++ MinutoSeguros.BlogFeed/MinutoSeguros.BlogFeed.Log/LoggerWrapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NLog;
+using MinutoSeguros.BlogFeed.Log.NLog;
+
+namespace MinutoSeguros.BlogFeed.Log
+{
+    public class LoggerWrapper : ILogger
+    {
+        private static Logger _logger;
+        
+        private static Logger Logger
+        {
+            get
+            {
+                return _logger ?? LogManager.GetCurrentClassLogger();
+            }
+        }
+        
+        private readonly NLog.ILogger _nlogLogger;
+
+        public LoggerWrapper()
+        {
+            _nlogLogger = new LoggerAdapter(Logger);
+        }
+
+        public void Info(string message)
+        {
+            _nlogLogger.Info(message);
+        }
+
+        public void Error(string message)
+        {
+            _nlogLogger.Error(message);
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            _nlogLogger.Error(message, exception);
+        }
+    }
+}
